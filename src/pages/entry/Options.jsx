c042bd884d3ff50aa443664c6f6a12fd221aa83b
harshageorge <harshageorge4@gmail.com>
2023-04-26 17:17:31 +0100

    Alert banner for options server error

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
-
-function Options({optionType}) {
-  return (
-    <div>Options</div>
-  )
+import { useEffect, useState } from "react";
+import ScoopOptions from "./ScoopOptions";
+import Row from "react-bootstrap/Row";
+// import axios from "axios";
+import ToppingOptions from "./ToppingOptions";
+import AlertBanner from "../common/AlertBanner";
+function Options({ optionType }) {
+  const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
+  // optionType is 'scoops' or 'toppings'
+  useEffect( () => {
+    // axios
+    //   .get(`https://localhost:3030/${optionType}`)
+    //   .then((response) => setItems(response.data))
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+    async function fetchData(){
+      await fetch(`http://localhost:3030/${optionType}`)
+.then((response) => response.json())
+.then((data) => setItems(data))
+.catch((error)=>{
+setError(true);
+});
+    }
+   fetchData();
+  }, [optionType]);
+  if(error){
+    return <AlertBanner/>
+  }
+  //Replace null with ToppingsOption when available
+  const ItemComponent = optionType === "scoops" ? ScoopOptions : ToppingOptions;
+  const optionItems = items.map((item) => <ItemComponent key={item.name} name={item.name} imagePath={item.imagePath}/>);
+  return <Row>{optionItems}</Row>;
 }
 
 export default Options;
--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -1,15 +1,28 @@
 import { render, screen } from "@testing-library/react";
 import Options from '../Options';
 
-test('display image for each scoop options from server',()=>{
+test('display image for each scoop options from server',async()=>{
     render(<Options optionType='scoops'/>);
 
     //find images
-    const scoopImages = screen.getAllByRole('img',{name: /scoop$/i});
+    const scoopImages = await screen.findAllByRole('img', { name: /scoop$/i});
     expect(scoopImages).toHaveLength(2);
 
     //confirm alt text of images
 
     const altText = scoopImages.map((element)=>element.alt);
     expect(altText).toEqual(['Chocolate scoop','Vanilla scoop']);
+})
+
+test('display image for each toppings options from server',async()=>{
+    render(<Options optionType='toppings'/>);
+
+    //find images
+    const toppingsImages = await screen.findAllByRole('img',{name: /topping$/i});
+    expect(toppingsImages).toHaveLength(3);
+
+    //confirm alt text of images
+
+    const altText = toppingsImages.map((element)=>element.alt);
+    expect(altText).toEqual(['Cherries topping','M&Ms topping','Hot fudge topping']);
 })
