efbe4ca6aa3e96a571785f349fbc16b767cee727
harshageorge <harshageorge4@gmail.com>
2023-04-27 14:37:50 +0100

    warning of unhandled request removed

--- src/pages/entry/tests/OrderEntry.test.jsx
+++ src/pages/entry/tests/OrderEntry.test.jsx
@@ -7,10 +7,14 @@ test("handles error for scoops and toppings route", async () => {
   server.resetHandlers(
     rest.get(
       "http://localhost:3030/scoops",
-      (req, res, ctx) => res(ctx.status(500)),
+      (req, res, ctx) => {
+        return res(ctx.status(500));
+      },
 
-      rest.get("http://localhost:3030/toppings", (req, res, ctx) =>
-        res(ctx.status(500))
+      rest.get("http://localhost:3030/toppings", (req, res, ctx) =>{
+  return  res(ctx.status(500));
+      }
+       
       )
     )
   );
